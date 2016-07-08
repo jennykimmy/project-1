92ff71e0b9b5fba638ac58823d5c5bc8357eb588
Jenny Kim <ms.jennykimmy@gmail.com>
2016-07-08 07:12:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -426,3 +426,11 @@ bcArray = questions2.bcArray;
       alert("You lose");
     })
   }
+
+  function winLogic2() {
+    if (bcArray.toString() === resultsArray.toString()) {
+      alert("You are correct!");
+    } else {
+      alert("ur wrong dummy");
+    }
+  }
